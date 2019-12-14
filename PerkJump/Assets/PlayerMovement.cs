1a7c04eae61ac039770e762bde520eb04d65a056
irenicor <52428327+irenicor@users.noreply.github.com>
2019-12-14 17:52:27 +0100

    Not more rookie numbers

--- PerkJump/Assets/PlayerMovement.cs
+++ PerkJump/Assets/PlayerMovement.cs
@@ -13,9 +13,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(10, 0, 0);
+            transform.position += new Vector3(0.5f, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-0.5f, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector3(0, 500, 0));
+            Debug.Log("Jump");
         }
     }
 }
